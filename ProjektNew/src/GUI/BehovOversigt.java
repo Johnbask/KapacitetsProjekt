c3ead5b4cbcd71bd57b33dc9fb368e41ed3cb56f
BEETSOLO <lbholm328b@gmail.com>
2026-05-12 14:57:25 +0200

    JavaFx for MedarbejderOversigt

--- ProjektNew/src/GUI/BehovOversigt.java
+++ ProjektNew/src/GUI/BehovOversigt.java
@@ -1,6 +1,37 @@
 package GUI;
 
+import Model.RessourceBehov;
 import javafx.scene.layout.GridPane;
 
+import java.awt.*;
+import java.util.ArrayList;
+
 public class BehovOversigt extends GridPane {
+
+    private GridPane timeline;
+    private ScrollPane søjleDia;
+
+    private ArrayList<RessourceBehov> ressourceBehov;
+
+
+    public BehovOversigt(){
+        initContent();
+        initAction();
+    }
+
+
+
+    public void initContent(){
+
+
+    }
+
+
+    public void initAction(){
+
+
+    }
+
+
+
 }
--- ProjektNew/src/GUI/TeamOversigt.java
+++ ProjektNew/src/GUI/TeamOversigt.java
@@ -1,33 +1,39 @@
 package GUI;
 
 
-
+import Model.Medarbejder;
 import Model.Team;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 import java.util.List;
 
-public class TeamOversigt extends GridPane {
+public class TeamOversigt extends BorderPane {
 
     private TableView<Team> tvwTeams;
+    private Button btnTilføjMedarbejder;
 
     public TeamOversigt() {
         initContent();
+        initActions();
     }
 
     private void initContent() {
 
+        this.setPadding(new Insets(10));
+
+        // ==========================================
+        // TABLE
+        // ==========================================
         tvwTeams = new TableView<>();
         tvwTeams.setPrefSize(500, 400);
-
-        // =====================================================
-        // COLUMNS
-        // =====================================================
 
         TableColumn<Team, Integer> colId =
                 new TableColumn<>("Team ID");
@@ -57,9 +63,91 @@ public class TeamOversigt extends GridPane {
                 colAntal
         );
 
-        this.add(tvwTeams, 0, 0);
+        this.setCenter(tvwTeams);
+
+        // ==========================================
+        // BUTTON BOTTOM LEFT
+        // ==========================================
+        btnTilføjMedarbejder = new Button("Tilføj medarbejder");
+
+        HBox bottomBox = new HBox();
+        bottomBox.setAlignment(Pos.BOTTOM_LEFT);
+        bottomBox.getChildren().add(btnTilføjMedarbejder);
+        bottomBox.setPadding(new Insets(10, 0, 0, 0));
+
+        this.setBottom(bottomBox);
     }
+
+    private void initActions() {
+
+        btnTilføjMedarbejder.setOnAction(e -> {
+
+            Team valgtTeam =
+                    tvwTeams.getSelectionModel().getSelectedItem();
+
+            if (valgtTeam != null) {
+                openTilføjMedarbejderWindow(valgtTeam);
+            }
+        });
+    }
+
     public void setTeams(List<Team> teams) {
         tvwTeams.getItems().setAll(teams);
+    }
+
+    private void openTilføjMedarbejderWindow(Team team) {
+
+        Stage stage = new Stage();
+        stage.setTitle("Tilføj medarbejder til " + team.getNavn());
+
+        GridPane pane = new GridPane();
+        pane.setPadding(new Insets(20));
+        pane.setHgap(10);
+        pane.setVgap(10);
+
+        TextField txfNavn = new TextField();
+        TextField txfInitialer = new TextField();
+        TextField txfStilling = new TextField();
+
+        pane.add(new Label("Navn:"), 0, 0);
+        pane.add(txfNavn, 1, 0);
+
+        pane.add(new Label("Initialer:"), 0, 1);
+        pane.add(txfInitialer, 1, 1);
+
+        pane.add(new Label("Stilling:"), 0, 2);
+        pane.add(txfStilling, 1, 2);
+
+        Button btnGem = new Button("Gem");
+        Button btnLuk = new Button("Luk");
+
+        pane.add(btnGem, 0, 3);
+        pane.add(btnLuk, 1, 3);
+
+        btnGem.setOnAction(e -> {
+
+            Medarbejder ny = new Medarbejder(
+                    999,
+                    txfInitialer.getText(),
+                    txfNavn.getText(),
+                    null,
+                    txfStilling.getText(),
+                    false,
+                    null,
+                    null,
+                    team
+            );
+
+            team.addMedarbejder(ny);
+
+            tvwTeams.refresh();
+            stage.close();
+        });
+
+        btnLuk.setOnAction(e -> stage.close());
+
+        Scene scene = new Scene(pane, 350, 220);
+        stage.setScene(scene);
+        stage.showAndWait();
     }
 }
